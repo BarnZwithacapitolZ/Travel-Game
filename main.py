e63e62bbea4510d1db8f8de2dd94b39d03761295
LucioFex <luciano1D2esteban@gmail.com>
2021-05-08 11:24:24 -0300

    refactor: Of the 'main.py' file

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import pygame
-import sys
-import os
-
-#insert directory paths
-sys.path.insert(0, 'menu')
-sys.path.insert(0, 'menu/functions')
-sys.path.insert(0, 'sprites')
-
+# Hey bro, import "*" is not a good practice
 from config import *
 from engine import *
 from spriteRenderer import *
@@ -14,8 +6,16 @@ from menu import *
 from sprites import *
 from menuComponents import *
 from mapEditor import *
+import pygame
+import sys
+# import os
+# import cProfile
 
-import cProfile
+
+# Insert directory paths
+sys.path.insert(0, 'menu')
+sys.path.insert(0, 'menu/functions')
+sys.path.insert(0, 'sprites')
 
 
 class Game:
@@ -29,7 +29,10 @@ class Game:
         self.playing = True
         self.clock = pygame.time.Clock()
         self.running = True
-        self.paused = True # start with the game paused (menus will keep running though)
+
+        # Start with the game paused (menus will keep running though)
+        self.paused = True
+
         self.fullscreen = config["graphics"]["fullscreen"]
         self.vsync = config["graphics"]["vsync"]
 
@@ -43,14 +46,13 @@ class Game:
         self.imageLoader = ImageLoader(self)
         self.mapLoader = MapLoader()
         self.audioLoader = AudioLoader()
-        
-        # Map editor 
+
+        # Map editor
         self.mapEditor = MapEditor(self)
 
         # Menu's
         self.mainMenu = MainMenu(self)
         self.optionMenu = OptionMenu(self)
-
 
         if self.fullscreen:
             self.renderer.setFullscreen()
@@ -68,25 +70,20 @@ class Game:
     def setCaption(self):
         pygame.display.set_caption(config["game"]["gameTitle"])
 
-
     # Set the games icon
     def setIcon(self):
         icon = self.imageLoader.getImage(config["game"]["icon"])
         pygame.display.set_icon(icon)
 
-
     # Set the games cursor (cursor class?)
     def setCursor(self):
         pygame.mouse.set_cursor(*pygame.cursors.tri_left)
 
-
     def getPaused(self):
         return self.paused
 
-
     def __quit(self):
         self.playing = False
-
 
     def __events(self):
         for e in pygame.event.get():
@@ -94,17 +91,22 @@ class Game:
                 self.__quit()
 
             elif e.type == pygame.VIDEORESIZE:
-                self.renderer.setScale(e.size, self.fullscreen)      
+                self.renderer.setScale(e.size, self.fullscreen)
 
             if e.type == pygame.KEYDOWN:
                 self.textHandler.events(e)
                 self.textHandler.setPressed(True)
 
-                # only open the option manu if the game isn't paused and the main menu isn't open
-                if e.key == pygame.K_ESCAPE and not self.mainMenu.open and not self.spriteRenderer.getMenu().open: 
-                    if not self.mapEditor.isDropdownsClosed(): # Close the dropdowns first
-                        if not self.optionMenu.open: self.optionMenu.main(True, True)
-                        else: self.optionMenu.closeTransition()
+                # Only open the option manu if the game isn't paused
+                # and the main menu isn't open
+                if (e.key == pygame.K_ESCAPE and not self.mainMenu.open
+                        and not self.spriteRenderer.getMenu().open):
+                    # Close the dropdowns first
+                    if not self.mapEditor.isDropdownsClosed():
+                        if not self.optionMenu.open:
+                            self.optionMenu.main(True, True)
+                        elif self.optionMenu.open:
+                            self.optionMenu.closeTransition()
 
                 elif e.key == pygame.K_RIGHT and self.mainMenu.open:
                     self.mainMenu.levelForward()
@@ -115,65 +117,75 @@ class Game:
                     if self.spriteRenderer.getHud().open:
                         self.spriteRenderer.getHud().togglePauseGame()
 
-                # if the game is not paused and the main menu isnt open and no text inputs are open
-                if not self.paused and not self.mainMenu.open and not self.textHandler.getActive():
+                # If the game is not paused and the main menu
+                # isnt open and no text inputs are open
+                if (not self.paused and not self.mainMenu.open
+                        and not self.textHandler.getActive()):
                     # Show / Hide the different layers depending on key press
                     if pygame.key.name(e.key) == config["controls"]["layer1"]:
                         self.spriteRenderer.showLayer(1)
                         self.mapEditor.showLayer(1)
 
-                    elif pygame.key.name(e.key) == config["controls"]["layer2"]:
+                    elif (pygame.key.name(e.key)
+                            == config["controls"]["layer2"]):
                         self.spriteRenderer.showLayer(2)
                         self.mapEditor.showLayer(2)
 
-                    elif pygame.key.name(e.key) == config["controls"]["layer3"]:
+                    elif (pygame.key.name(e.key)
+                            == config["controls"]["layer3"]):
                         self.spriteRenderer.showLayer(3)
                         self.mapEditor.showLayer(3)
 
-                    elif pygame.key.name(e.key) == config["controls"]["layer4"]:
+                    elif (pygame.key.name(e.key)
+                            == config["controls"]["layer4"]):
                         self.spriteRenderer.showLayer(4)
                         self.mapEditor.showLayer(4)
 
-                if e.key == pygame.K_z and pygame.key.get_mods() & pygame.KMOD_CTRL and not self.paused and not self.mainMenu.open:
+                if (e.key == pygame.K_z
+                    and pygame.key.get_mods() & pygame.KMOD_CTRL
+                        and not self.paused and not self.mainMenu.open):
                     self.mapEditor.undoChange()
                     level = self.mapEditor.getLevelData()
                     layer = self.mapEditor.getLayer()
-                    self.mapEditor.createLevel(level, layer = layer) #reload the level
+                    # Reload the level
+                    self.mapEditor.createLevel(level, layer=layer)
 
-                elif e.key == pygame.K_y and pygame.key.get_mods() & pygame.KMOD_CTRL and not self.paused and not self.mainMenu.open:
+                elif (e.key == pygame.K_y
+                        and pygame.key.get_mods() & pygame.KMOD_CTRL
+                        and not self.paused and not self.mainMenu.open):
                     self.mapEditor.redoChange()
                     level = self.mapEditor.getLevelData()
                     layer = self.mapEditor.getLayer()
-                    self.mapEditor.createLevel(level, layer = layer) #reload the level
-
+                    # Reload the level
+                    self.mapEditor.createLevel(level, layer=layer)
 
             elif e.type == pygame.KEYUP:
                 self.textHandler.setPressed(False)
 
-
-            # Just for fun :) 
+            # Just for fun :) - And refactoring ( ͡° ͜ʖ ͡°)
             if e.type == pygame.MOUSEBUTTONDOWN:
                 if e.button == 4:
-                    self.spriteRenderer.setFixedScale(self.spriteRenderer.getFixedScale() + 0.1)
+                    self.spriteRenderer.setFixedScale(
+                        self.spriteRenderer.getFixedScale() + 0.1)
                     self.spriteRenderer.resize()
-                    
-                elif e.button == 5: 
-                    self.spriteRenderer.setFixedScale(self.spriteRenderer.getFixedScale() - 0.1)
+
+                elif e.button == 5:
+                    self.spriteRenderer.setFixedScale(
+                        self.spriteRenderer.getFixedScale() - 0.1)
                     self.spriteRenderer.resize()
 
             # Make left click set the destination on the click manager instead
             if e.type == pygame.MOUSEBUTTONDOWN:
                 if e.button == 1:
-                    self.clickManager.setClicked(True) 
+                    self.clickManager.setClicked(True)
                 elif e.button == 3:
                     self.clickManager.setRightClicked(True)
             else:
                 self.clickManager.setClicked(False)
                 self.clickManager.setRightClicked(False)
-                    
 
     def run(self):
-        #main menu
+        # Main menu
         self.mainMenu.main()
 
         # Game loop
@@ -190,24 +202,26 @@ class Game:
 
         self.running = False
 
-
     def __update(self):
         # print(self.paused)
         if not self.paused and not self.mainMenu.open:
             self.spriteRenderer.update()
             self.mapEditor.update()
 
-
     def __draw(self):
-        #add sprites
-        if self.mainMenu.open: self.renderer.prepareSurface(self.mainMenu.getBackgroundColor())
+        # Add sprites
+        if self.mainMenu.open:
+            self.renderer.prepareSurface(self.mainMenu.getBackgroundColor())
 
         self.spriteRenderer.render()
         self.mapEditor.render()
 
-        #add menus when not paused
-        if self.paused: self.optionMenu.display() #To Do: Different option menus for sprite renderer and level editor
-        if self.mainMenu.open: self.mainMenu.display()
+        # Add menus when not paused
+        if self.paused:
+            # To Do:Different option menus for sprite renderer and level editor
+            self.optionMenu.display()
+        if self.mainMenu.open:
+            self.mainMenu.display()
 
         # render everything
         self.renderer.render()
